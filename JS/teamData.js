f1512c83107fa241dc7c2584e88a0d5eed38e29b
Evans Sitibekiso <jevacrism@gmail.com>
2022-03-17 22:32:58 +0200

    add icons and about.html

--- JS/teamData.js
+++ JS/teamData.js
@@ -1,73 +1,73 @@
 const data = [
   {
     id: 1,
-    name: 'Albert Noah',
+    name: "Evans Sitibekiso",
     images: {
-      img: '../images/member1.png',
-      alt: 'team_1',
+      img: "../images/icons/evans.jpeg",
+      alt: "team_1",
     },
-    about: 'President Gen. of Cloudflare Company.',
-    work: 'Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.',
+    about: "Ceo jevacrism Group of Companies.",
+    work: "Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.",
   },
   {
     id: 2,
-    name: 'Albert Noah',
+    name: "Evans Sitibekiso",
     images: {
-      img: '../images/member2.png',
-      alt: 'team_2',
+      img: "../images/icons/evans.jpeg",
+      alt: "team_2",
     },
-    about: 'President Gen. of Cloudflare Company.',
-    work: 'Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.',
+    about: "Ceo Dana Oil.",
+    work: "Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.",
   },
   {
     id: 3,
-    name: 'Albert Noah',
+    name: "Sofia lerner",
     images: {
-      img: '../images/member3.png',
-      alt: 'team_3',
+      img: "../images/staff3.png",
+      alt: "team_3",
     },
-    about: 'President Gen. of Cloudflare Company.',
-    work: 'Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.',
+    about: "Global Entrepreneur.",
+    work: "Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.",
   },
   {
     id: 4,
-    name: 'Albert Noah',
+    name: "Patricia jackson",
     images: {
-      img: '../images/member4.png',
-      alt: 'team_4',
+      img: "../images/staff4.png",
+      alt: "team_4",
     },
-    about: 'President Gen. of Cloudflare Company.',
-    work: 'Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.',
+    about: "Global Speaker.",
+    work: "Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.",
   },
   {
     id: 5,
-    name: 'Albert Noah',
+    name: "Marry Korey",
     images: {
-      img: '../images/member3.png',
-      alt: 'team_3',
+      img: "../images/staff5.png",
+      alt: "team_3",
     },
-    about: 'President Gen. of Cloudflare Company.',
-    work: 'Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.',
+    about: "Ceo Chena Oil Campany.",
+    work: "Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.",
   },
   {
     id: 6,
-    name: 'SohYeong Noh',
+    name: "Eric yen",
     images: {
-      img: '../images/member6.png',
-      alt: 'team_6',
+      img: "../images/staff6.png",
+      alt: "team_6",
     },
-    about: 'Head Manager of designers Company.',
-    work: 'Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.',
+    about: "Global Speaker.",
+    work: "Lorem ipsum, consectetur adipisicing elit.Perferendis asperiores, quo distinctio, atque, quasi nobis.",
   },
 ];
 
-const teamContainer = document.querySelector('#Team .teams');
+const teamContainer = document.querySelector("#Team .teams");
 
-// Implementing the Flex card dynamically
+// Flex card dynamically
 
 function createCard(dataInfo) {
-  const li = document.createElement('li');
-  li.className = 'card d-flex';
+  const li = document.createElement("li");
+  li.className = "card d-flex";
   li.innerHTML = `<div class="card-img">
     <img src="${dataInfo.images.img}" alt="${dataInfo.images.alt}">
   </div>
@@ -84,38 +84,38 @@ data.forEach((info) => {
   teamContainer.appendChild(card);
 });
 
-// Implementing the Flex card dynamically
+// Flex card dynamically
 
-const teamList = document.querySelectorAll('#Team .teams li');
-const moreBtn = document.querySelector('#Team button');
-const btnSpan = document.querySelector('#Team button span');
-const btnIcon = document.querySelector('#Team button i');
+const teamList = document.querySelectorAll("#Team .teams li");
+const moreBtn = document.querySelector("#Team button");
+const btnSpan = document.querySelector("#Team button span");
+const btnIcon = document.querySelector("#Team button i");
 
 let state = false;
 
 function displayMore() {
   if (!state) {
     teamList.forEach((team) => {
-      team.style.display = 'flex';
+      team.style.display = "flex";
     });
-    btnSpan.textContent = 'LESS';
-    btnIcon.classList.remove('fa', 'fa-chevron-down');
-    btnIcon.classList.add('fa', 'fa-chevron-up');
+    btnSpan.textContent = "LESS";
+    btnIcon.classList.remove("fa", "fa-chevron-down");
+    btnIcon.classList.add("fa", "fa-chevron-up");
 
     state = true;
   } else {
     teamList.forEach((team) => {
-      team.style.display = 'none';
+      team.style.display = "none";
     });
-    teamList[0].style.display = 'flex';
-    teamList[1].style.display = 'flex';
+    teamList[0].style.display = "flex";
+    teamList[1].style.display = "flex";
 
-    btnSpan.textContent = 'MORE';
-    btnIcon.classList.remove('fa', 'fa-chevron-up');
-    btnIcon.classList.add('fa', 'fa-chevron-down');
+    btnSpan.textContent = "MORE";
+    btnIcon.classList.remove("fa", "fa-chevron-up");
+    btnIcon.classList.add("fa", "fa-chevron-down");
 
     state = false;
   }
 }
 
-moreBtn.addEventListener('click', displayMore);
+moreBtn.addEventListener("click", displayMore);
